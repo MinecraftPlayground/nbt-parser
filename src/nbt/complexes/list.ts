504b49ac85c2d123b5396f9dcd404f0f960a4df8
Mqx <62719703+Mqxx@users.noreply.github.com>
2025-03-28 17:51:48 +0100

    Update list.ts

--- src/nbt/complexes/list.ts
+++ src/nbt/complexes/list.ts
@@ -1,11 +1,27 @@
-import { PrimitiveType } from "../primitives/index.ts";
-import { Compound } from "./compound.ts";
+import type {
+  Byte,
+  Double,
+  Float,
+  Int,
+  Long,
+  Short
+} from "../primitives/index.ts";
+
+import type { Compound } from "./compound.ts";
 
 /**
  * This type represents a NBT List.
  */
-export type List<Type extends
-  | List<Type>
-  | Compound
-  | PrimitiveType = Compound | PrimitiveType
-> = Type[];
+export type List =
+  | List[]
+  | Compound[]
+  | Byte[]
+  | Short[]
+  | Int[]
+  | Long[]
+  | Float[]
+  | Double[]
+  | string[]
+  | Byte[][]
+  | Int[][]
+  | Long[][];
