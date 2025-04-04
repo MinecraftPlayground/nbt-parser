e94f040e04f7448b4aa9834ddac5be127e8652cc
Mqx <62719703+Mqxx@users.noreply.github.com>
2025-04-04 20:33:28 +0200

    Update compound.ts

--- src/nbt/complexes/compound.ts
+++ src/nbt/complexes/compound.ts
@@ -2,9 +2,8 @@ import type { PrimitiveType } from "../primitives/index.ts";
 import type { List } from "./list.ts";
 
 /** This type represents a NBT Compound. */
-export interface Compound {
-  [key : string] :
-    | Compound
-    | List
-    | PrimitiveType
-}
+export type Compound = {
+  '': List | PrimitiveType;
+} & {
+  [key: string]: Compound | List | PrimitiveType;
+};
