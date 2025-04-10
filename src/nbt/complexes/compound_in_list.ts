130dd3a74d83082ffde3eba75d2043732c27e34c
Mqxx <62719703+Mqxx@users.noreply.github.com>
2025-04-10 15:54:08 +0200

    Update compound_in_list.ts

--- src/nbt/complexes/compound_in_list.ts
+++ src/nbt/complexes/compound_in_list.ts
@@ -3,11 +3,6 @@ import type { Compound } from "./compound.ts";
 import type { List } from "./list.ts";
 
 /** This type represents a NBT Compound in a List. */
-export type CompoundInList = { '':
-  | List
-  | PrimitiveType
-} & { [key: string]:
-  | Compound
-  | List
-  | PrimitiveType
-};
+export type CompoundInList = {
+  [key : string] : typeof key extends '' ? CompoundInList : List | PrimitiveType;
+}
