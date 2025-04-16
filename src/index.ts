add4c614678629bce4cabc737be5702ce5b20edf
Mqx <62719703+Mqxx@users.noreply.github.com>
2025-04-16 21:39:04 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,1 @@
-import type { NBT } from "./nbt/index.ts";
 
-const data : NBT.Data = {
-  name: "test",
-  myList: [1, 2, 3],
-  myOtherList: [
-    {
-      '': 1
-    },
-    {
-      '': 1,
-      a: []
-    },
-    {
-      b: {}
-    },
-    {}
-  ]
-}
