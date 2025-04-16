b362e39e95ed44b9968c468b5a97cf15215294dc
Mqx <62719703+Mqxx@users.noreply.github.com>
2025-04-16 21:47:27 +0200

    Refactor

--- src/nbt/complex/compound.ts
+++ src/nbt/complex/compound.ts
@@ -2,9 +2,11 @@ import type { SimpleType } from "../simple/index.ts";
 import type { List } from "./list.ts";
 
 /** This type represents a NBT Compound. */
-export interface Compound {
-  [key: string]:
+export type Compound = {
+  [key : string] :
     | Compound
     | List
     | SimpleType
-};
+} & {
+  ''? : never
+}
--- src/nbt/complex/compound_in_list.ts
+++ src/nbt/complex/compound_in_list.ts
@@ -2,24 +2,17 @@ import type { SimpleType } from "../simple/index.ts";
 import type { Compound } from "./compound.ts";
 import type { List } from "./list.ts";
 
+/** This type represents an empty NBT Compound. */
 type EmptyCompound = {
   [key : string] : never;
 }
 
+/** This type represents a NBT Compound wrapper in a List. */
 type CompoundWrapper = {
   '' :
     | List
     | SimpleType
 }
 
-type AnyKeyCompound = {
-  [key : string] :
-    | Compound
-    | List
-    | SimpleType
-} & {
-  ''? : never
-}
-
 /** This type represents a NBT Compound in a List. */
-export type CompoundInList = EmptyCompound | CompoundWrapper | AnyKeyCompound
+export type CompoundInList = EmptyCompound | CompoundWrapper | Compound
